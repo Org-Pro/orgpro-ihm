2572f0d4f9d4b919fb306ad4c6c754265e6cba31
Thibault Sartre <32135105+ThibaultSartre@users.noreply.github.com>
2018-02-13 00:59:15 +0100

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-start cmd /k java -classpath build/classes/java/main fr.orgpro.ihm.project.Main
+start cmd /k java -classpath build/classes/java/main;libs/orgpro-api-0.1.1-SNAPSHOT.jar fr.orgpro.ihm.project.Main
