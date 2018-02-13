c2f199dbc4b8cc7715705ba5bfd82ad2c869a1fc
WALLOIS-Alexandre <32265304+WALLOIS-Alexandre@users.noreply.github.com>
2018-02-13 09:06:10 +0100

    Update Message.java

--- src/main/java/fr/orgpro/ihm/project/Message.java
+++ src/main/java/fr/orgpro/ihm/project/Message.java
@@ -18,12 +18,12 @@ public enum Message {
     TACHES_NON_IDENTIQUES_ECHEC("ECHEC : Les tâches indiquées ne doivent pas être identiques."),
     TACHE_HELP("TACHE ADD <Nom de la tâche> <Niveau de la tâche> -> Ajoute une tâche\n" +
             "TACHE LIST -> Affiche les tâches\n" +
-            "TACHE SET DEP <Numéro de la tâche qui reçoit> <Numéro de la tâche qui donne> -> Ajoute une dépendance à la tâche\n" +
+            "TACHE DEP SET <Numéro de la tâche qui reçoit> <Numéro de la tâche qui donne> -> Ajoute une dépendance à la tâche\n" +
             "TACHE DEP REMOVE <Numéro de la tâche> -> Supprime la dépendance de la tâche.\n" +
             "TACHE RENAME <Numéro de la tâche> <Nouveau titre> -> Change le titre de la tâche.\n" +
             "TACHE LEVEL <Numéro de la tâche> <Niveau de la tâche> -> Change le niveau de la tâche.\n" +
             "TACHE CLOCK RESET <Numéro de la tâche> -> Remet à zéro le minuteur de la tâche.\n" +
-            "TACHE CLOCK ... <Numéro de la tâche> -> Lance ou stop le minuteur de la tâche."),
+            "TACHE CLOCK USE <Numéro de la tâche> -> Lance ou stop le minuteur de la tâche."),
 
     FICHIER_SAVE_SUCCES("SUCCES : Fichier enregistré.")
     ;
