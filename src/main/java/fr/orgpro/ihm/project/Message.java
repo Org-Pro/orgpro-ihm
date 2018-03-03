721f1ec8820ad9e9b8fbb8f7f45d6a83f338f77f
Thibault Sartre <sartre.thibault@hotmail.fr>
2018-03-03 19:45:52 +0100

    Ajout message en anglais

--- src/main/java/fr/orgpro/ihm/project/Message.java
+++ src/main/java/fr/orgpro/ihm/project/Message.java
@@ -1,68 +1,68 @@
 package fr.orgpro.ihm.project;
 
 public enum Message {
-    COMMANDE_INCONNUE("ECHEC : La commande est inconnue."),
-    ARGUMENT_MANQUANT("ECHEC : Il manque des arguments."),
-    ARGUMENT_INVALIDE("ECHEC : Un des arguments n'est pas valide."),
-    PROBLEME_LECTURE("ECHEC : Problème lors de la lecture du fichier."),
+    COMMANDE_INCONNUE("FAILURE : Unknown command."),
+    ARGUMENT_MANQUANT("FAILURE : Missing argument(s)."),
+    ARGUMENT_INVALIDE("FAILURE : One of the argument is invalid."),
+    PROBLEME_LECTURE("FAILURE : Trouble reading the file."),
 
     MAIN_HELP(""),
-    MAIN_AUNCUN_FICHIER("Vous devez créer ou charger un fichier.\n" +
-            "Commande : FILE SELECT <Nom du fichier à créer / charger>"),
-    MAIN_LISTE_FICHIER("Vous pouvez charger un fichier parmi la liste :"),
+    MAIN_AUNCUN_FICHIER("You must create or upload a file.\n" +
+            "Command : FILE SELECT <Name of the file to create / load>"),
+    MAIN_LISTE_FICHIER("You can load a file from the list :"),
 
-    TACHE_AJOUT_SUCCES("SUCCES : Tâche créée."),
-    TACHE_AJOUT_DEADLINE_SUCCES("SUCCES : Deadline ajoutée/modifiée."),
-    TACHE_AJOUT_CLOSED_SUCCES("SUCCES : Closed ajoutée/modifiée."),
-    TACHE_AJOUT_SCHEDULED_SUCCES("SUCCES : Scheduled ajoutée/modifiée."),
-    TACHE_AJOUT_TAG_SUCCES("SUCCES : Tag ajouté."),
-    TACHE_AJOUT_PROPRIETE_SUCCES("SUCCES : Property ajoutée."),
-    TACHE_DELETE_PROPRIETE_SUCCES("SUCCES : Property supprimée."),
-    TACHE_DELETE_TAG_SUCCES("SUCCES : Tag supprimé."),
-    TACHE_DELETE_SUCCES("SUCCES : Tâche supprimée."),
-    TACHE_DELETE_DEADLINE_SUCCES("SUCCES : Deadline supprimée."),
-    TACHE_DELETE_CLOSED_SUCCES("SUCCES : Closed supprimée."),
-    TACHE_DELETE_SCHEDULED_SUCCES("SUCCES : Scheduled supprimée."),
-    TACHE_MINUTEUR_RESET_SUCCES("SUCCES : Minuteur de la tâche remit à zéro."),
-    TACHE_MINUTEUR_LANCER_SUCCES("SUCCES : Minuteur lancé."),
-    TACHE_MINUTEUR_STOPPER_SUCCES("SUCCES : Minuteur stoppé."),
-    TACHE_SET_DEPENDANCE_SUCCES("SUCCES : La dépendance a bien été ajoutée / modifiée."),
-    TACHE_RENAME_SUCESS("SUCCES : La tâche a bien été renommée."),
-    TACHE_DELETE_DEPENDANCE_SUCCES("SUCCES : La dépendance a bien été supprimée."),
-    TACHE_LEVEL_SUCCES("SUCCES : Le niveau de la tâche a bien été changé."),
-    TACHE_AJOUT_PROPRIETE_ECHEC("ECHEC : La property ne doit pas contenir : ID et DEPENDENCE."),
-    TACHE_SET_DEPENDANCE_ECHEC("ECHEC : La dépendance ne peut pas être modifiée."),
-    TACHE_DELETE_DEPENDANCE_ECHEC("ECHEC : La dépendance ne peut pas être supprimée."),
-    TACHE_DELETE_PROPRIETE_ECHEC("ECHEC : La property ne doit pas contenir : ID et DEPENDENCE."),
-    TACHE_LEVEL_DEP_ECHEC("ECHEC : Une dépendance empêche le changement de niveau."),
-    TACHE_LEVEL_NEGATIF_ECHEC("ECHEC : Le niveau de la tâche doit être supérieur ou égal à 1."),
-    TACHE_INVALIDE_ECHEC("ECHEC : Numéro de tâche invalide."),
-    TACHES_NON_IDENTIQUES_ECHEC("ECHEC : Les tâches indiquées ne doivent pas être identiques."),
-    TACHE_AJOUT_DEADLINE_ECHEC("ECHEC : Le format de la date est incorrect. (AAAA/MM/JJ)"),
-    TACHE_AJOUT_CLOSED_ECHEC("ECHEC : Le format de la date est incorrect. (AAAA/MM/JJ)"),
-    TACHE_AJOUT_SCHEDULED_ECHEC("ECHEC : Le format de la date est incorrect. (AAAA/MM/JJ)"),
-    TACHE_HELP("TACHE ADD <Nom de la tâche> -> Ajoute une tâche\n" /*+
-            "TACHE LIST -> Affiche les tâches\n" +
-            "TACHE DEP SET <Numéro de la tâche qui reçoit> <Numéro de la tâche qui donne> -> Ajoute une dépendance à la tâche\n" +
-            "TACHE DEP REMOVE <Numéro de la tâche> -> Supprime la dépendance de la tâche.\n" +
-            "TACHE RENAME <Numéro de la tâche> <Nouveau titre> -> Change le titre de la tâche.\n" +
-            "TACHE LEVEL <Numéro de la tâche> <Niveau de la tâche> -> Change le niveau de la tâche.\n" +
-            "TACHE CLOCK RESET <Numéro de la tâche> -> Remet à zéro le minuteur de la tâche.\n" +
-            "TACHE CLOCK USE <Numéro de la tâche> -> Lance ou stop le minuteur de la tâche."*/),
+    TACHE_AJOUT_SUCCES("SUCCESS : Task created."),
+    TACHE_AJOUT_DEADLINE_SUCCES("SUCCESS : Deadline add/modified."),
+    TACHE_AJOUT_CLOSED_SUCCES("SUCCESS : Closed add/modified."),
+    TACHE_AJOUT_SCHEDULED_SUCCES("SUCCESS : Scheduled add/modified."),
+    TACHE_AJOUT_TAG_SUCCES("SUCCESS : Tag add."),
+    TACHE_AJOUT_PROPRIETE_SUCCES("SUCCESS : Property add."),
+    TACHE_DELETE_PROPRIETE_SUCCES("SUCCESS : Property deleted."),
+    TACHE_DELETE_TAG_SUCCES("SUCCESS : Tag deleted."),
+    TACHE_DELETE_SUCCES("SUCCESS : Task deleted."),
+    //TACHE_DELETE_DEADLINE_SUCCES("SUCCES : Deadline supprimée."),
+    //TACHE_DELETE_CLOSED_SUCCES("SUCCES : Closed supprimée."),
+    //TACHE_DELETE_SCHEDULED_SUCCES("SUCCES : Scheduled supprimée."),
+    //TACHE_MINUTEUR_RESET_SUCCES("SUCCES : Minuteur de la tâche remit à zéro."),
+    //TACHE_MINUTEUR_LANCER_SUCCES("SUCCES : Minuteur lancé."),
+    //TACHE_MINUTEUR_STOPPER_SUCCES("SUCCES : Minuteur stoppé."),
+    TACHE_SET_DEPENDANCE_SUCCES("SUCCESS : Dependency has been added / modified."),
+    TACHE_RENAME_SUCESS("SUCCESS : The task has been renamed."),
+    TACHE_DELETE_DEPENDANCE_SUCCES("SUCCESS : Dependency deleted."),
+    //TACHE_LEVEL_SUCCES("SUCCES : Le niveau de la tâche a bien été changé."),
+    TACHE_AJOUT_PROPRIETE_ECHEC("FAILURE : Cannot add property ID or DEPENDENCE."),
+    TACHE_SET_DEPENDANCE_ECHEC("FAILURE : Dependency cannot be updated."),
+    TACHE_DELETE_DEPENDANCE_ECHEC("FAILURE : Dependency cannot be deleted."),
+    TACHE_DELETE_PROPRIETE_ECHEC("FAILURE : Cannot delete property ID or DEPENDENCE."),
+    //TACHE_LEVEL_DEP_ECHEC("ECHEC : Une dépendance empêche le changement de niveau."),
+    //TACHE_LEVEL_NEGATIF_ECHEC("ECHEC : Le niveau de la tâche doit être supérieur ou égal à 1."),
+    TACHE_INVALIDE_ECHEC("FAILURE : Invalid task number."),
+    TACHES_NON_IDENTIQUES_ECHEC("FAILURE : tasks must not be identical."),
+    TACHE_AJOUT_DEADLINE_ECHEC("FAILURE : Wrong date format. (YYYY/MM/DD)."),
+    TACHE_AJOUT_CLOSED_ECHEC("FAILURE : Wrong date format. (YYYY/MM/DD)."),
+    TACHE_AJOUT_SCHEDULED_ECHEC("FAILURE : Wrong date format. (YYYY/MM/DD)."),
+    TACHE_HELP("TASK ADD <Title of task> -> Add a task\n"
 
-    FICHIER_SAVE_SUCCES("SUCCES : Fichier enregistré."),
-    FICHIER_CREATION("SUCCES : Le fichier n'existe pas mais il a bien été créé."),
-    FICHIER_LOAD("SUCCES : Chargement du fichier réussi."),
-    FICHIER_LISTE("Liste des fichiers disponibles :"),
-    FICHIER_LISTE_VIDE("Auncun fichier trouvé."),
 
-    LIST_AUCUN_RESULTAT("Aucun résultat(s)."),
+
+    //TODO
+
+
+    ),
+
+    //FICHIER_SAVE_SUCCES("SUCCESS : Saved file."),
+    FICHIER_CREATION("SUCCESS : File does not exist but it was created."),
+    FICHIER_LOAD("SUCCESS : Loaded file."),
+    FICHIER_LISTE("List of file available :"),
+    FICHIER_LISTE_VIDE("No file found."),
+
+    LIST_AUCUN_RESULTAT("No result."),
     LIST_HELP("LIST TASK -> displays the tasks of a file\n" +
     "LIST TS <State> (TODO,ONGOING,DONE,CANCELLED) -> displays the tasks that correspond to the given state"),
 
-    STATE_UPDATE_SUCCES("SUCCES : State modifiée."),
-    STATE_UPDATE_ECHEC("ECHEC : State non modifiée."),
-    STATE_INTROUVABLE("ECHEC : La State n'existe pas.")
+    STATE_UPDATE_SUCCES("SUCCESS : Changed State."),
+    STATE_UPDATE_ECHEC("FAILURE : Unchanged STate."),
+    STATE_INTROUVABLE("FAILURE : State does not exist.")
     ;
 
     private String message;
