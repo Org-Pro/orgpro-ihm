ab1c23040ea69c42bb9e64d142798f1ed1c4aa70
Baptiste Rayer <batrayer77@gmail.com>
2018-11-17 13:56:04 +0100

    Feat: ajout d'une fonction pour insérer les identifiants trello d'un user (token + apikey) + ajout d'une fonction permettant la synchro de toutes les taches d'un user à trello

--- src/main/java/fr/orgpro/ihm/project/GoogleIhm.java
+++ src/main/java/fr/orgpro/ihm/project/GoogleIhm.java
@@ -3,17 +3,19 @@ package fr.orgpro.ihm.project;
 import fr.orgpro.api.project.State;
 import fr.orgpro.api.project.Tache;
 import fr.orgpro.api.remote.google.GoogleList;
+import fr.orgpro.ihm.service.CollaborateurService;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class CollaborateurIhm {
-    private static CollaborateurIhm INSTANCE = new CollaborateurIhm();
+public class GoogleIhm {
+    private static GoogleIhm INSTANCE = new GoogleIhm();
+    private static CollaborateurService cl = CollaborateurService.getInstance();
     private static final GoogleList gl = GoogleList.getInstance();
 
     public void syncUser(String name, Data data){
         List<Tache> toSend;
-        toSend = getListTacheFromCollabo(name, data);
+        toSend = cl.getListTacheFromCollabo(name, data);
         if(toSend != null) {
             for(Tache t : toSend) {
                 gl.postTache(name, t.getTitre(), t.getDateLimite());
@@ -24,7 +26,7 @@ public class CollaborateurIhm {
 
     public void syncStatusTaskUser(String name, Data data, State etat){
         List<Tache> toSend;
-        toSend = getListTacheFromCollabo(name, data);
+        toSend = cl.getListTacheFromCollabo(name, data);
         for(Tache t : toSend) {
             if(t.getEtat().equals(etat)) {
                 gl.postTache(name, t.getTitre(), t.getDateLimite());
@@ -32,20 +34,7 @@ public class CollaborateurIhm {
         }
     }
 
-    private List<Tache> getListTacheFromCollabo(String name, Data data) {
-        List<Tache> toSend = new ArrayList<>();
-        for (Tache task: data.getListeTache()) {
-            for(String c : task.getCollaborateurFromTache(task)) {
-                if (c.equalsIgnoreCase(name)) {
-                    toSend.add(task);
-                    break;
-                }
-            }
-        }
-        System.out.println(toSend.get(0).toString());
-        return toSend;
-    }
-    public static CollaborateurIhm getInstance() {
+    public static GoogleIhm getInstance() {
         return INSTANCE;
     }
 }
